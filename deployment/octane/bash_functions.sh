bad62d057ffb1ba3fe0d9136868ab9f2453383b5
mortexa <seyed.me720@gmail.com>
2021-10-19 16:41:08 +0330

    [fix] bash syntax

--- deployment/octane/bash_functions.sh
+++ deployment/octane/bash_functions.sh
@@ -7,6 +7,6 @@ tinker() {
     if [ -z "$1" ]; then
         php artisan tinker
     else
-        php artisan tinker --execute="dd($1);"
+        php artisan tinker --execute="\"dd($1);\""
     fi
 }
