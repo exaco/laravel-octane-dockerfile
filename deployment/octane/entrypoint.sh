852b1ab3407c29ee0835ee0c820244a9c6c12376
mortexa <seyed.me720@gmail.com>
2021-10-20 14:24:28 +0330

    [fix] bash syntax

--- deployment/octane/entrypoint.sh
+++ deployment/octane/entrypoint.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 set -e
 
-initialStuff(){
-    su octane -c "\
-        php artisan event:cache; \
-        php artisan config:cache; \
-        php artisan route:cache;"
+initialStuff() {
+    php artisan event:cache; \
+    php artisan config:cache; \
+    php artisan route:cache;
 }
 
 if [ "$1" != "" ]; then
