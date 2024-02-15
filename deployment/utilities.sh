6b509de67d149352f6a32aa386cc165b3adedd85
Mortexa <47121888+smortexa@users.noreply.github.com>
2024-02-16 02:12:54 +0330

    Minor Improvements (#60)

--- deployment/utilities.sh
+++ deployment/utilities.sh
@@ -6,20 +6,6 @@ tinker() {
   fi
 }
 
-# Determine size of a file or total size of a directory
-fs() {
-  if du -b /dev/null >/dev/null 2>&1; then
-    local arg=-sbh
-  else
-    local arg=-sh
-  fi
-  if [[ -n "$@" ]]; then
-    du $arg -- "$@"
-  else
-    du $arg .[^.]* ./*
-  fi
-}
-
 # Commonly used aliases
 alias ..="cd .."
 alias ...="cd ../.."
